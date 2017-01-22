8d51e2477c4abb2fc5e098a57bd008d28f74296d
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2017-01-22 16:07:55 +0100

    --wip-- [skip ci]

--- app/app.js
+++ app/app.js
@@ -9,6 +9,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var http = require('http');
+var io = require('socket.io')(1923);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +27,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+io.on('connection', function (socket) {
+  console.log(socket);
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/src/main/main.js
+++ app/src/main/main.js
@@ -2,17 +2,45 @@ import Vue from 'vue'
 import Listable from '../components/listable.js'
 import Grocery from '../models/Grocery.js'
 import Groceries from '../models/Groceries.js'
+import VueSocketio from 'vue-socket.io'
 
-var list = new Listable('todo-item', 'todo', 'getText()');
-var groceries = new Groceries();
-
-groceries.addGrocery(new Grocery('Vegetables'));
-groceries.addGrocery(new Grocery('Cheese'));
-groceries.addGrocery(new Grocery('Potatoes'));
-
-var app7 = new Vue({
-  el: '#app-7',
+var a = Vue.use(VueSocketio, 'http://localhost:1923');
+// var b = new Vue(a);
+var vm = new Vue({
+  el : "#chat",
   data: {
-    groceryList: groceries.getGroceries()
+    message: '',
+    listMessages: []
+  },
+  sockets:{
+    connect: function(){
+      console.log('socket connected')
+    },
+    newMessageEmit: function(val){
+      console.log(val);
+
+    }
+  },
+  methods: {
+    sendMessage: function(val){
+        // $socket is socket.io-client instance
+        this.$socket.emit('my other event', this.message);
+        this.message = '';
+    }
   }
 });
+
+vm.use = a;
+
+// var list = new Listable('todo-item', 'todo', 'getText()');
+// var groceries = new Groceries();
+
+// groceries.addGrocery(new Grocery('Vegetables'));
+// groceries.addGrocery(new Grocery('Cheese'));
+// groceries.addGrocery(new Grocery('Potatoes'));
+// var app7 = new Vue({
+//   el: '#app-7',
+//   data: {
+//     groceryList: groceries.getGroceries()
+//   }
+// });
